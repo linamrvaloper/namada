07ceb34885e21a49ba132be6745400d13ea7677a
linamr <106906306+linamrvaloper@users.noreply.github.com>
2024-04-06 20:52:40 +0400

    Update restake.sh

--- restaking/restake.sh
+++ restaking/restake.sh
@@ -16,6 +16,8 @@ check_balance() {
     if [[ $balance -lt $stop_balance ]]; then
         echo "The balance of $WALLET is less than minimum of $stop_balance. Current balance: $balance."
         return 1  # Indicate failure due to low balance
+    else
+        echo "Current balance: $balance"
     fi
     return 0  # Indicate success
 }
@@ -23,8 +25,11 @@ check_balance() {
 
 # Function to restake validator rewards
 validator_restake() {
+  echo "Attempting to restake validator rewards..."
   val_reward=$(echo "$(namadac rewards --validator "$VALIDATOR_ADDRESS")" | grep -o '[0-9]\+\.[0-9]\+')
+  echo "Validator rewards available for claiming: $val_reward"
   if check_balance && [[ $(echo "$val_reward > $min_bond" | bc) -eq 1 ]] && [[ $(echo "$balance + $val_reward > $min_balance + $min_bond" | bc) -eq 1 ]]; then
+  echo "Proceeding to claim rewards..."
     claim_output=$(expect -c "set timeout -1
                   spawn namadac claim-rewards --validator "$VALIDATOR_ADDRESS" --memo "$MEMO"
                   expect \"Enter your decryption password: \"
@@ -46,24 +51,36 @@ validator_restake() {
         if echo "$bond_output" | grep -q "Transaction was successfully applied"; then
           echo "Successfully bonded "$bond_amount" NAM to validator "$VALIDATOR_ADDRESS"."
           echo "_________________________________________________________________________"
+        else
+          echo "Error: Failed to bond."
+          return 1
         fi            
+      else
+        echo "Error: Bond amount ($bond_amount) is less than the minimum bond ($min_bond)."
+        return 1
       fi
     fi
-    return 0
+  else
+    echo "Error: Pre-conditions for validator restake not met."
+    return 1
   fi
-  return 1
+  return 0
 }
 
 
 wallet_restake() {
-# ...
+    echo "Wallet restaking should be here..."
+    # ...
 }
 
 # Infinite loop to repeat the main loop every 4 hours
 while true; do
     validator_restake
+    echo "_________________________________________________________________________"
     wallet_restake
+    echo "_________________________________________________________________________"
     echo "Sleeping for 1 hour..."
-    echo "-----------------------------------------------------------------"
+    echo "_________________________________________________________________________"
+    echo "_________________________________________________________________________"
     sleep 1h
  done
